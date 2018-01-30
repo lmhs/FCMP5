767556652da624437a7611132195f18ec7598a66
Maksim Labovich <maksim_labovich@epam.com>
2018-01-31 01:50:49 +0300

    Added put

--- app.js
+++ app.js
@@ -18,9 +18,12 @@ app.get('/blogs', (req, res) => {
   res.send(JSON.stringify(data));
 });
 
+function getPost(id) {
+  return data.articles.find((article) => article.id === +id)
+}
+
 app.get('/blogs/:id', (req, res) => {
-  const id = +req.params.id;
-  const article = data.articles.find((item) => item.id === id);
+  const article = getPost(req.params.id);
   res.send(`<article><h2>${article.title}</h2> <h3>${article.author}</h3> <p>${article.content}</p>`);
 });
 
@@ -39,8 +42,25 @@ app.post('/blogs', (req, res) => {
   fs.writeFile('Articles.json', JSON.stringify(newData));
 });
 
+app.put('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  const article = getPost(id);
+  // immutable article
+  const newArticle = Object.assign({}, article, req.body);
+
+  // immutable array
+  const articles = [
+    ...data.articles.slice(0, id),
+    newArticle,
+    ...data.articles.slice(id + 1)
+  ];
+  const newData = {articles};
+
+  fs.writeFile('Articles.json', JSON.stringify(newData));
+});
+
 function removePost(id) {
-  return data.articles.filter((article) => article.id !== id);
+  return data.articles.filter((article) => article.id !== +id);
 }
 
 app.delete('/blogs/:id', (req, res) => {
