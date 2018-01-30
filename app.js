df71bc7fbe490c15778d28d060dfe061ea8bbd60
Maksim Labovich <maksim_labovich@epam.com>
2018-01-30 23:16:46 +0300

    Added nodemon

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const config = require('./config.json');
 const article = require('./Article.json');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`${JSON.stringify(article)}`);
-}).listen(config.port);
+app.get('/', (req, res) => res.send(JSON.stringify(article)));
+
+app.listen(config.port, () => console.log(`Listening to ports: ${config.port}`));
