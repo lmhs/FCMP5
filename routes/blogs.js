1f0efca7da2930fabfd5eeb924b6d7ec1754181b
Maksim Labovich <maksim_labovich@epam.com>
2018-02-07 02:03:37 +0300

    removed unused packages

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const jwt = require('jsonwebtoken');
 const config = require('../utils/config.json');
 
